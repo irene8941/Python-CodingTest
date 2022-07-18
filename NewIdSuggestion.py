70b2a66abecbb92124cdf5c7079f8d64eea5316e
irene8941 <irene8941@naver.com>
2022-07-18 23:56:56 +0900

    [2] 신규 아이디 추천

--- NewIdSuggestion.py
+++ NewIdSuggestion.py
@@ -1,28 +1,19 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/72410
+
+import re
+
 def solution(new_id):
     answer = ''
-    # 1단계
-    new_id = new_id.lower()
-    # 2단계
-    for n in new_id:
-        if n.isalnum() or n in '-_.':
-            answer += n
+    # 1&2단계
+    answer = re.sub('[^0-9a-z_.-]+', '', new_id.lower())
     # 3단계
-    while '..' in answer:
-        answer = answer.replace("..", ".")
+    answer = re.sub('[..]+', '.', answer)
     # 4단계
-    if answer[0] == '.' and len(answer) > 1:
-        answer = answer[1 :]
-    if answer[-1] == '.':
-        answer = answer[: -1]
+    answer = answer.strip('.')
     # 5단계
-    if answer == "":
-        answer = "a"
+    answer = "a" if answer == "" else answer
     # 6단계
-    if len(answer) >= 16:
-        answer = answer[:15]
-        if answer[-1] == '.':
-            answer = answer[ : len(answer) - 1]
-    print("6:", answer)
+    answer = answer[:15].rstrip('.')
     # 7단계
     if len(answer) <= 2:
         while len(answer) < 3:
