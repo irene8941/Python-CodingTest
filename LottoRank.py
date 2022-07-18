f2c7227f7d89e9cea7c8b19d41bb0bdcb574bd9c
irene8941 <irene8941@naver.com>
2022-07-18 20:27:19 +0900

    [2] 로또의 최고 순위와 최저 순위

--- LottoRank.py
+++ LottoRank.py
@@ -1,17 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/77484
 
 def solution(lottos, win_nums):
-    answer = []
-    todo = list(set(win_nums) - set(lottos))
-    todoLen = len(todo)
-    zeroCnt = lottos.count(0)
+    answer = 0
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    cnt0 = lottos.count(0)
     
-    if todoLen == 0:
-        answer = [1, 1]
-    elif zeroCnt == 6:
-        answer = [1, 6]
-    elif todoLen == 6 and zeroCnt == 0:
-        answer = [6, 6]
-    else:
-        answer = [1+(todoLen-zeroCnt), todoLen + 1]
-    return answer
+    for w in win_nums:
+        if w in lottos:
+            answer += 1
+    
+    return [rank[answer + cnt0], rank[answer]]
