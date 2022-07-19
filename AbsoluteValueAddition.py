4d9433b154b59a44597ad92901cc579d98e8311d
irene8941 <irene8941@naver.com>
2022-07-19 12:43:50 +0900

    [2] 음양 더하기

--- AbsoluteValueAddition.py
+++ AbsoluteValueAddition.py
@@ -1,12 +1,4 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/76501
 
 def solution(absolutes, signs):
-    answer = 0
-    
-    for i in range(len(absolutes)):
-        if signs[i]:
-            answer += absolutes[i]
-        else:
-            answer -= absolutes[i]
-
-    return answer
+    return sum(absolute if sign else -absolute for absolute, sign in zip(absolutes, signs))
