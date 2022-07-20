f253023c12a8f0fc3b007be8ffdbae2b888e200c
irene8941 <irene8941@naver.com>
2022-07-20 14:25:45 +0900

    [1] 소수 만들기

---
+++ MakePrimeNumber.py
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12977
+
+from itertools import combinations
+import math
+
+def solution(nums):
+    nums.sort()
+    sets = list(combinations(nums, 3))
+    answer = len(sets)
+    
+    for s in sets:
+        sumSet = sum(s)
+        for i in range(2, int(math.sqrt(sumSet)) + 1):
+            if sumSet % i == 0:
+                answer -= 1
+                break
+
+    return answer
