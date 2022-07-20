13b167e5b49a673f9f7e81480e5dc442d4bdf65f
irene8941 <irene8941@naver.com>
2022-07-20 17:38:00 +0900

    [1] 숫자 문자열과 영단어

---
+++ MixedStringToNumber.py
@@ -0,0 +1,29 @@
+def solution(s):
+    answer = ""
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    a = 0
+    while a < len(s):
+        # 이미 숫자인 경우
+        if s[a].isdigit():
+            answer += str(s[a])
+            a += 1
+        # 영어인 경우
+        else:
+            word = s[a]
+            for b in range(a + 1, len(s)):
+                if s[b].isdigit():
+                    answer += str(words.index(word))
+                    a = b + 1
+                    break
+                else:
+                    word += s[b]
+                    if word in words:
+                        answer += str(words.index(word))
+                        a = b + 1
+                        break
+
+    
+    return int(answer)
+
+solution("one4seveneight")
