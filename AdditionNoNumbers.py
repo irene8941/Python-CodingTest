c9ccd8c77d34eb976350d69e014bfc4638a422d1
irene8941 <irene8941@naver.com>
2022-07-22 11:41:18 +0900

    [2] 없는 숫자 더하기

--- AdditionNoNumbers.py
+++ AdditionNoNumbers.py
@@ -1,5 +1,4 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/86051
 
 def solution(numbers):
-    numSets = [ i for i in range(10)]
-    return sum(list(set(numSets) - set(numbers)))
+    return sum([i for i in range(10)]) - sum(numbers)
