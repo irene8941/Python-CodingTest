63fb4e4987a622a802a1572aaea2d1cd641a7406
irene8941 <irene8941@naver.com>
2022-09-03 21:21:54 +0900

    [2] 시저 암호

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,17 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12926
 
 def solution(s, n):
-    alpha = [0, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
     answer = ""
+
     for char in s:
-        if not char.isalpha():
+        if not char.isalpha():  # 공백
             answer += char
-        else:
-            idx = alpha.index(char.upper()) + n - 26 if alpha.index(char.upper()) + n > 26 else alpha.index(char.upper()) + n
-            if char.isupper():
-                answer += alpha[idx]
-            else:
-                answer += alpha[idx].lower()
+            continue
+        if char.isupper():      # 대문자
+            answer += chr((ord(char) - ord('A') + n) % 26 + ord('A'))
+        else:                   # 소문자
+            answer += chr((ord(char) - ord('a') + n) % 26 + ord('a'))
 
     return answer
