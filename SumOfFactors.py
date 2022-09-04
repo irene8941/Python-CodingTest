c4154b35beb949a321f620b571ef7c330dea91ec
irene8941 <irene8941@naver.com>
2022-09-04 20:54:28 +0900

    [1] 약수의 합

---
+++ SumOfFactors.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12928
+
+def solution(n):
+    answer = 0
+
+    for i in range(1, n + 1):
+        answer += i if n % i == 0 else 0
+
+    return answer
