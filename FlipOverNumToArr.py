993c0c184b1804a6876de8e3a9af82f9d2e97f0d
irene8941 <irene8941@naver.com>
2022-09-04 22:33:21 +0900

    [2] 자연수 뒤집어 배열로 만들기

--- FlipOverNumToArr.py
+++ FlipOverNumToArr.py
@@ -1,7 +1,4 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12932
 
 def solution(n):
-    arr = [int(i) for i in str(n)]
-    arr.reverse()
-
-    return arr
+    return list(map(int, reversed(str(n))))
