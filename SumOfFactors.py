e03313d362509b9f89acbd84dea4bd2ac3698915
irene8941 <irene8941@naver.com>
2022-09-04 21:03:02 +0900

    [2] 약수의 합

--- SumOfFactors.py
+++ SumOfFactors.py
@@ -1,9 +1,4 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12928
 
 def solution(n):
-    answer = 0
-
-    for i in range(1, n + 1):
-        answer += i if n % i == 0 else 0
-
-    return answer
+    return n + sum([i for i in range(1, n // 2 + 1) if n % i == 0])
