207ce822078985618980f616f0b4988b7671305a
irene8941 <irene8941@naver.com>
2022-09-16 21:47:17 +0900

    [1] 네트워크

---
+++ Network.py
@@ -0,0 +1,23 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/43162
+
+def solution(n, computers):
+    visited = [False] * n
+    answer = 0
+
+    def dfs(start):
+        stack = [start]
+        while stack:
+            node = stack.pop()
+            if visited[node]:
+                continue
+            visited[node] = True
+            for j in range(n):
+                if computers[node][j] == 1:
+                    stack.append(j)
+
+    for i in range(n):
+        if not visited[i]:
+            dfs(i)
+            answer += 1
+
+    return answer
