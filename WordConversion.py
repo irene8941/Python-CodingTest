a5caab0eb2730269a074bcb22bad1942cb3a461a
irene8941 <irene8941@naver.com>
2022-09-16 22:33:48 +0900

    [2] 단어 변환

--- WordConversion.py
+++ WordConversion.py
@@ -1,29 +1,31 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43163
 
-def diff(now, new):
+def diff(now, nxt):
     count = 0
-    for i, j in zip(now, new):
-        if i != j:
-            count += 1
-    return True if count == 1 else False
-
-def func(begin, target, words, visited):
-    stack = [begin]
-    count = -1
-    while stack:
-        now = stack.pop()
+    for w, t in zip(now, nxt):
+        if w == t:
+            continue
         count += 1
-        if now == target:
-            return count
-        for idx in range(len(words)):
-            if not visited[idx] and diff(now, words[idx]):
-                visited[idx] = True
-                stack.append(words[idx])
+    return count
 
 def solution(begin, target, words):
+    visited = [False for _ in range(len(words))]
+    count = -1
+
     if target not in words:
         return 0
 
-    visited = [False for _ in range(len(words))]
+    stack = [begin]
+    while stack:
+        now = stack.pop()
+        count += 1
+        if target == now:
+            return count
+        for idx in range(len(words)):
+            if visited[idx]:
+                continue
+            if diff(now, words[idx]) == 1:
+                   visited[idx] = True
+                   stack.append(words[idx])
 
-    return func(begin, target, words, visited)
+    return count
