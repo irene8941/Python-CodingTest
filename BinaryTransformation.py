50643f1309c0bc0e593bcafa91555ed8ab41cce8
irene8941 <irene8941@naver.com>
2022-09-18 21:28:12 +0900

    [2] 이진 변환 반복하기

--- BinaryTransformation.py
+++ BinaryTransformation.py
@@ -4,24 +4,10 @@ def solution(s):
     cnt0 = 0    # 제거한 0의 개수
     step = 0    # 이진 변환 횟수
 
-    while s != "1":
-        # x의 모든 0제거
-        if s.count("0") > 0:
-            cnt0 += s.count("0")
-            s = s.replace("0", "")
-        # 0 제거 후 길이
-        s = len(s)
-        # 이진 변환 결과
-        temp = []
+    while s != '1':
         step += 1
-        while s > 1:
-            if s % 2 == 0:
-                temp.append("0")
-            else:
-                temp.append("1")
-            s //= 2
-        temp.append("1")
-        temp.reverse()
-        s = "".join(temp)
+        cnt0 += s.count('0')    # 제거된 0의 개수
+        cnt1 = s.count('1')     # 0제거 > 길이
+        s = bin(cnt1)[2:]       # 2진법 변환
 
     return [step, cnt0]
