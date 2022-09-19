f576b63644ef90ba10db65fb9da0dd8391dd0333
irene8941 <irene8941@naver.com>
2022-09-19 21:55:01 +0900

    [1] 예상 대진표

---
+++ ExpectedDraw.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12985
+
+def solution(n,a,b):
+    answer = 0
+    while a != b:
+        answer +=1
+        a, b = (a + 1) // 2, (b + 1) // 2
+    return answer
