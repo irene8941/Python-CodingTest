0efd3307caa0c4674cf8557d76006d5101853838
irene8941 <irene8941@naver.com>
2022-09-19 13:24:45 +0900

    [1] 다음 큰 숫자

--- NextBigNum.py
+++ NextBigNum.py
@@ -1,8 +1,9 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/12911
-
 def solution(n):
+    cnt = bin(n)[2 : ].count('1')
+
     # 조건1
-    for i in range(n+ 1, 1000000):
+    for i in range(n + 1, 1000000):
         # 조건2
-        if bin(i)[2:].count('1') == bin(n)[2:].count('1'):
+        if bin(i)[2 : ].count('1') == cnt:
+            # 조건3
             return i
