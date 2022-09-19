9ff24ae5f104918173667686d368b40a9cdbe9b6
irene8941 <irene8941@naver.com>
2022-09-19 23:00:30 +0900

    [2] 멀리뛰기

--- RunFar.py
+++ RunFar.py
@@ -1,12 +1,12 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12914#
 
 def solution(n):
-    if n < 3:
-        return n
-
     d = [0] * (n + 1)
-    d[1] = 1
-    d[2] = 2
+    try:
+        d[1] = 1
+        d[2] = 2
+    except IndexError:
+        return n
 
     for i in range(3, n + 1):
         d[i] = (d[i - 1] + d[i - 2]) % 1234567
