5f75cd6cef832328d4275a203e2373a304f2e82c
irene8941 <irene8941@naver.com>
2022-09-20 23:55:15 +0900

    [1] 단지번호붙이기

--- ApartmentComplexNumbering.py
+++ ApartmentComplexNumbering.py
@@ -1,46 +1,42 @@
-# https://www.acmicpc.net/problem/2667
-
 from collections import deque
+
+# BFS 구현
 def bfs(x, y):
-    cnt = 0
-    queue = deque([(x, y)])    # 큐
-    visited[x][y] = 1
+    queue = deque([(x, y)])
+    graph[x][y] = 0
+    cnt = 0 # 단지 내 집의 수
     while queue:
         x, y = queue.popleft()
         cnt += 1
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            # 지도를 벗어난 경우
-            if nx < 0 or ny < 0 or nx >= N or ny >= N:
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 지도의 범위를 벗어난 경우
+            if nx < 0 or ny < 0 or nx >= n or ny >= n:
                 continue
-            if graph[nx][ny] == 1 and visited[nx][ny] == 0:
+            # 방문하지 않은 아파트인 경우
+            if graph[nx][ny] == 1:
                 queue.append((nx, ny))
-                visited[nx][ny] = 1
+                graph[nx][ny] = 0
     return cnt
 
 # 지도의 크기
-N = int(input())
+n = int(input())
 # 지도
-# 1: 집이 있는 곳
-# 0: 집이 없는 곳 또는 이미 방문한 집
 graph = []
-for _ in range(N):
+for _ in range(n):
     graph.append(list(map(int, input())))
-# 방문처리
-visited = [[0] * N for _ in range(N)]
-# 방문방향(상,하,좌,우)
+# 방향(상,하,좌,우)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-totCnt = 0
-cntArr = []
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 1 and visited[i][j] == 0:
-            totCnt += 1
-            cntArr.append(bfs(i, j))
-cntArr.sort()
+arr = []
+for x in range(n):
+    for y in range(n):
+        if graph[x][y] == 1:
+            arr.append(bfs(x, y))
 
-print(len(cntArr))
-for a in cntArr:
+print(len(arr))
+arr.sort()
+for a in arr:
     print(a)
