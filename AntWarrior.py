6b8b1e5264f94588c8d9e05394557b858569930b
irene8941 <irene8941@naver.com>
2022-09-23 23:56:36 +0900

    [2] 개미 전사

--- AntWarrior.py
+++ AntWarrior.py
@@ -1,13 +1,18 @@
 #  이것이코딩테스트다 p.220 개미 전사
 
-n = int(input())    # 식량창고의 개수
-k = list(map(int, input().split()))   # 각 식량창고에 저장된 식량의 개수
-d = [0] * n
+# 식량창고의 개수
+n = int(input())
+# 각 식량창고에 저장된 식량의 개수
+arr = list(map(int, input().split()))
 
-for idx in range(n):
-    if idx <= 1:
-        d[idx] = k[idx]
-    else:
-        d[idx] = max(d[idx - 1], d[idx - 2] + k[idx])
+# DP 테이블 초기화
+d = [0] * 100
 
-print(max(d))
+# 다이나믹 프로그래밍 진행(상향법)
+d[0] = arr[0]
+d[1] = max(arr[0], arr[1])
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + arr[i])
+
+# 계산된 결과 출력
+print(d[n - 1])
