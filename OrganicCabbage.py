9ec1cdc344d7ebb81db3141a3678047070e22fb9
irene8941 <irene8941@naver.com>
2023-05-09 20:03:24 +0900

    [2] 유기농 배추

--- OrganicCabbage.py
+++ OrganicCabbage.py
@@ -1,45 +1,42 @@
-# https://www.acmicpc.net/problem/1012
+import sys
+sys.setrecursionlimit(10000)
 
-from collections import deque
+def dfs(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 배추밭 범위를 벗어나는 경우
+        if nx < 0 or ny < 0 or nx >= m or ny >= n:
+            continue
+        # 한 번도 방문하지 않은 배추인 경우
+        if graph[nx][ny] == 1:
+            graph[nx][ny] = 0
+            dfs(nx, ny)
 
-def bfs(x, y, graph, m, n):
-    queue = deque([(x, y)])
-    graph[x][y] = 0
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= m or ny >= n:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx, ny))
-
-# 테스트 케이스의 개수 T
+# 테스트 케이스의 개수 t
 t = int(input())
-# 테스트 케이스 별 배추희지렁이 마리 수
 answer = []
 
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
 for _ in range(t):
-    # 배추밭의 가로길이 M, 세로길이 N, 배추가 심어져 있는 위치의 개수 K
+    # 배추밭의 가로길이 m, 세로길이 n, 배추가 심어져 있는 위치의 개수 k
     m, n, k = map(int, input().split())
-    # 그래프
-    graph = [[0] * n for _ in range(m)]
-    # 배추의 위치
+
+    graph = [[0] * n for _ in range(m)] # 배추밭
     for _ in range(k):
+        # 배추의 위치 (x, y)
         x, y = map(int, input().split())
         graph[x][y] = 1
-    # 방문방향
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
 
     cnt = 0
-    for x in range(m):
-        for y in range(n):
-            if graph[x][y] == 1:
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j] == 1:
                 cnt += 1
-                bfs(x, y, graph, m, n)
+                graph[i][j] = 0
+                dfs(i, j)
     answer.append(cnt)
 
 for i in answer: print(i)
